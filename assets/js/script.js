2a6cf27dfcd14cf4c69ee57269f36a4179e1e5f6
ShaliniS FEBootCamper <118174874+ShaliniSunkuru@users.noreply.github.com>
2024-01-20 21:58:37 +0000

    cleaned up code into functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,31 @@
-var hourCol = $(".hour");
 
-var rowNumber = 1;
+setCurrentDay();
+setHours();
 
-//p element to display current day
-var currentDayP = $('#currentDay'); 
+function setCurrentDay() {
+    //p element to display current day
+    var currentDayP = $('#currentDay');
 
-//get today's day using dayjs library
-var currentDay = dayjs().format("D MMMM YYYY");
+    //get today's day using dayjs library
+    var currentDay = dayjs().format("D MMMM YYYY");
 
-currentDayP.text(currentDay);
-
-//set time for the timeblocks
-
-var startHour = dayjs().set('hour', 8).set('minute', 0).set('second', 0);
-// console.log(startHour.format("D M YYYY [at] h mm ss a"));
-var numberOfWorkingHours = 9;
-for(var i = 1; i <= numberOfWorkingHours; i++){
-    var thisHour = startHour.add(i, 'hour');
-    console.log(thisHour.format("D M YYYY [at] h mm ss a"));
-    var hourRow=$("#hour"+i);
-    hourRow.children(".hour").text(thisHour.format("h a"));
-    // hourCol.text(thisHour.format("h a"));
+    currentDayP.text(currentDay);
 }
+
+function setHours() {
+    //set time on the time column on timeblocks
+    var startHour = dayjs().set('hour', 8).set('minute', 0).set('second', 0);
+
+    var numberOfWorkingHours = 9;
+
+    for (var i = 1; i <= numberOfWorkingHours; i++) {
+        var thisHour = startHour.add(i, 'hour');
+        console.log(thisHour.format("D M YYYY [at] h mm ss a"));
+        var hourRow = $("#hour" + i);
+        hourRow.children(".hour").text(thisHour.format("h a"));
+    }
+}
+
 
 
 
